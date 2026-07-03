69684e41b1c04a5de5d86e69577bf16905f5e306
Job King <jobezking@gmail.com>
2026-07-03 11:48:04 -0400

    update

--- refresher.py
+++ refresher.py
@@ -1,6 +1,22 @@
 print("Jesus Christ is Lord")
 outline = "Jesus Christ is the Son of God and Savior of humanity."
-print(outline)
+print("It is true that" + outline)
 
 for i in range(5):
     print(f"Iteration {i + 1}: Remember to follow the teachings of Jesus Christ.")
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+
+number = int(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+def to_celsius(x):
+    return (x-32) * 5/9
+
+to_celsius(75)
