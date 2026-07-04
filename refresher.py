3c35b7e2d867a08c23a7edc6e465698a6fec94c4
Job King <jobezking@gmail.com>
2026-07-04 18:22:41 -0400

    test

--- refresher.py
+++ refresher.py
@@ -44,6 +44,24 @@ while x < 5:
     x = x + 1
 print("x=" + str(x))
 
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+  chars += len(animal)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+
+winners = ["Ashley", "Dylan", "Reese"]
+for index, person in enumerate(winners):
+  print("{} - {}".format(index + 1, person))
+
+def full_emails(people):
+  result = []
+  for email, name in people:
+    result.append("{} <{}>".format(name, email))
+  return result
+print(full_emails([("alex@example.com", "Alex Diego"), ("shay@example.com", "Shay Brandt")]))
+
 def get_username():
     username = input("Enter your username: ")
     return username
