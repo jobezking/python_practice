152cba3f3d2ff2a606c7c7ab5dc86a5c828845e2
Job King <jobezking@gmail.com>
2026-07-07 20:33:30 -0400

    push

--- files.py
+++ files.py
@@ -20,5 +20,25 @@ file.close()
 lines.sort()
 print(lines)
 
+f = open("sample_data/declaration.txt", "w")
+f = open('workfile', 'w', encoding="utf-8") 
+
 with open("novel.txt", "w") as file:
     file.write("It was a dark and stormy night")
+
+with open("novel.txt", "w") as file:
+    file.write("It was a dark and stormy night")
+    
+"""
+The mode argument is optional, and it specifies the mode in which the file is opened. If omitted, it defaults to ”r” and that means opening for reading in text mode. The common modes include:
+
+“r”  open for reading (default)
+
+“w”  open for writing, truncating the file first
+
+“x”  open for exclusive creation, failing if the file already exists
+
+“a”  open for writing, appending to the end of the file if it exists
+
+“+”  open for both reading and writing
+"""
