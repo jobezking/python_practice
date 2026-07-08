eff433a3339cdcc04f132f8d2fe4de258cc26586
Job King <jobezking@gmail.com>
2026-07-07 22:56:56 -0400

    push

--- google_automation/regex.py
+++ google_automation/regex.py
@@ -4,3 +4,72 @@ log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package 
 regex = r"\[(\d+)\]"
 result = re.search(regex, log)
 print(result[1])
+
+result = re.search(r"aza", "plaza")
+print(result)
+
+result = re.search(r"aza", "maze")
+print(result)
+
+print(re.search(r"^x", "xenon"))
+
+print(re.search(r"p.ng", "penguin"))
+print(re.search(r"p.ng", "clapping"))
+print(re.search(r"p.ng", "sponge"))
+print(re.search(r"p.ng", "Pangaea", re.IGNORECASE))
+
+print(re.search(r"[Pp]ython", "Python"))
+
+print(re.search(r"[a-z]way", "The end of the highway"))
+print(re.search(r"[a-z]way", "What a way to go"))
+print(re.search("cloud[a-zA-Z0-9]", "cloudy"))
+print(re.search("cloud[a-zA-Z0-9]", "cloud9"))
+
+print(re.search(r"[^a-zA-Z]", "This is a sentence with spaces."))
+print(re.search(r"[^a-zA-Z ]", "This is a sentence with spaces."))
+
+print(re.search(r"cat|dog", "I like cats."))
+print(re.search(r"cat|dog", "I love dogs!"))
+print(re.search(r"cat|dog", "I like both dogs and cats."))
+
+print(re.search(r"cat|dog", "I like cats."))
+print(re.search(r"cat|dog", "I love dogs!"))
+print(re.search(r"cat|dog", "I like both dogs and cats."))
+print(re.findall(r"cat|dog", "I like both dogs and cats."))
+
+print(re.search(r"Py.*n", "Pygmalion"))
+print(re.search(r"Py.*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Pyn"))
+
+print(re.search(r"o+l+", "goldfish"))
+print(re.search(r"o+l+", "woolly"))
+print(re.search(r"o+l+", "boil"))
+
+print(re.search(r"p?each", "To each their own"))
+print(re.search(r"p?each", "I like peaches"))
+
+print(re.search(r".com", "welcome"))
+print(re.search(r"\.com", "welcome"))
+print(re.search(r"\.com", "mydomain.com"))
+
+print(re.search(r"\w*", "This is an example"))
+print(re.search(r"\w*", "And_this_is_another"))
+
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern, "_this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
+
+# r"\d{3}-\d{3}-\d{4}"  This line of code matches U.S. phone numbers in the format 111-222-3333.
+
+# r"^-?\d*(\.\d+)?$"  This line of code matches any positive or negative number, with or without decimal places.
+
+# r"^/(.+)/([^/]+)/$" This line of code is often used to extract specific parts of URLs or file paths, such as the directory names or filenames.
+
+# https://regex101.com/
