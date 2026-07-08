ec2a0ba79887bad6b565ad28d35a78b8bb66cb32
Job King <jobezking@gmail.com>
2026-07-07 21:41:35 -0400

    push

--- files.py
+++ files.py
@@ -69,11 +69,24 @@ for row in csv_f:
    print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
 f.close()
 
+with open('software.csv') as software:
+    reader = csv.DictReader(software)
+    for row in reader:
+      print(("{} has {} users").format(row["name"], row["users"]))
+
 hosts = [["workstation.local", "192.168.25.46"],["webserver.cloud", "10.2.5.6"]]
 with open('hosts.csv', 'w') as hosts_csv:
     writer = csv.writer(hosts_csv)
     writer.writerows(hosts)
 
+users = [ {"name": "Sol Mansi", "username": "solm", "department": "IT infrastructure"}, 
+ {"name": "Lio Nelson", "username": "lion", "department": "User Experience Research"}, 
+  {"name": "Charlie Grey", "username": "greyc", "department": "Development"}]
+keys = ["name", "username", "department"]
+with open('by_department.csv', 'w') as by_department:
+    writer = csv.DictWriter(by_department, fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(users)
 
 #The mode argument is optional, and it specifies the mode in which the file is opened. If omitted, it defaults to ”r” and that means opening for reading in text mode. The common modes include:
 
