872442700acc77e99790f31ae5af41c866414e24
Job King <jobezking@gmail.com>
2026-07-07 20:48:12 -0400

    push

--- files.py
+++ files.py
@@ -1,3 +1,5 @@
+import os
+
 file = open("spider.txt")
 print(file.readline())
 print(file.readline())
@@ -28,17 +30,20 @@ with open("novel.txt", "w") as file:
 
 with open("novel.txt", "w") as file:
     file.write("It was a dark and stormy night")
+
+#CWD command for external files:
+outputs['current_directory_before'] = os.getcwd()
     
 """
 The mode argument is optional, and it specifies the mode in which the file is opened. If omitted, it defaults to ”r” and that means opening for reading in text mode. The common modes include:
 
-“r”  open for reading (default)
+"r"  open for reading (default)
 
-“w”  open for writing, truncating the file first
+"w" open for writing, truncating the file first
 
-“x”  open for exclusive creation, failing if the file already exists
+"x"  open for exclusive creation, failing if the file already exists
 
-“a”  open for writing, appending to the end of the file if it exists
+"a"  open for writing, appending to the end of the file if it exists
 
-“+”  open for both reading and writing
+"+"  open for both reading and writing
 """
