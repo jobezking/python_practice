73b0c267a84fa386271a4d2f2b5dc041c4b3f4d3
Job King <jobezking@gmail.com>
2026-07-17 18:39:24 -0400

    push

---
+++ google_automation/unittest/more_edge.py
@@ -0,0 +1,35 @@
+from google_automation.unittest.unit_test_ex import rearrange_name
+
+import unittest
+import re
+
+class TestRearrange(unittest.TestCase):
+  
+  def test_basic(self):
+    testcase = "Lovelace, Ada"
+    expected = "Ada Lovelace"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_empty(self):
+    testcase = ""
+    expected = ""
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_double_name(self):
+    testcase = "Hopper, Grace M."
+    expected = "Grace M. Hopper"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_one_name(self):
+    testcase = "Voltaire"
+    expected = "Voltaire"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def rearrange_name(name):
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result is None:
+      return name
+    return "{} {}".format(result[2], result[1])
+
+# Run the tests
+unittest.main()
