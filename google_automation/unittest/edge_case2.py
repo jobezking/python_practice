902d2f24da955d22c0565f71a57e9a1dd85f0d28
Job King <jobezking@gmail.com>
2026-07-17 18:21:37 -0400

    push

---
+++ google_automation/unittest/edge_case2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from google_automation.unittest.unit_test_ex import rearrange_name
+
+#!/usr/bin/env python3
+
+import re
+
+def rearrange_name(name):
+  result = re.search(r"^([\w .-]*), ([\w .-]*)$", name)
+  if result is None:
+    return ""
+  return "{} {}".format(result[2], result[1])
+
+# ./rearrange_test.py 
---
+++ google_automation/unittest/edge_cases.py
@@ -0,0 +1,9 @@
+from google_automation.unittest.unit_test_ex import rearrange_name
+
+def test_empty(self):
+  testcase = ""
+  expected = ""
+  self.assertEqual(rearrange_name(testcase), expected)
+
+# ./rearrange_test.py 
+
