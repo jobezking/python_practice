afbbbdc4718a375eada7ba74b22b9ccd84d1feba
Job King <jobezking@gmail.com>
2026-07-17 20:35:00 -0400

    push

---
+++ google_automation/pytest/tryexcept.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+def character_frequency(filename):
+  """Counts the frequency of each character in the given file."""
+  # First try to open the file
+  try:
+    f = open(filename)
+  except OSError:
+    return None
+
+  # Now process the file
+  characters = {}
+  for line in f:
+    for char in line:
+      characters[char] = characters.get(char, 0) + 1
+  f.close() 
+  return characters
+
+def validate_user(username, minlen):
+  if minlen < 1:
+    raise ValueError("minlen must be at least 1")
+
+  if len(username) < minlen:
+    return False
+  if not username.isalnum():
+    return False
+  return True
+
+
+def validate_user2(username, minlen):
+  assert type(username) == str, "username must be a string"
+  if minlen < 1:
+    raise ValueError("minlen must be at least 1")
+
+  if len(username) < minlen:
+    return False
+  if not username.isalnum():
+    return False
+  return True
---
+++ google_automation/pytest/validations_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from google_automation.pytest.tryexcept import validate_user2
+
+class TestValidateUser(unittest.TestCase):
+  def test_valid(self):
+    self.assertEqual(validate_user2("validuser", 3), True)
+
+  def test_too_short(self):
+    self.assertEqual(validate_user2("inv", 5), False)
+
+  def test_invalid_characters(self):
+    self.assertEqual(validate_user2("invalid_user", 1), False)
+  def test_invalid_minlen(self):
+    self.assertRaises(ValueError, validate_user2, "user", -1)
+
+
+# Run the tests
+unittest.main()
